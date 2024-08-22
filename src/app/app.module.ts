83cd999cc5b607a2e0c630d2f0454c6c4a221aab
PabloSnhz <pablosanhuezaaa@gmail.com>
2024-08-21 23:48:05 -0300

    reqresApi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { AgGridUseComponent } from './pages/ag-grid-use/ag-grid-use.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AgGridAngular } from 'ag-grid-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserListComponent } from './pages/reactiveMaterial/user-list/user-list.component';
+import { UserFormComponent } from './pages/reactiveMaterial/user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AgGridUseComponent
+    AgGridUseComponent,
+    UserListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    AgGridAngular
+    AgGridAngular,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
