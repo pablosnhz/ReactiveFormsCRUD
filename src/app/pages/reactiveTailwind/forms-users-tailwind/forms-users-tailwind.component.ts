eb798686699fa842e2964ba88591cc603f9f74c7
PabloSnhz <pablosanhuezaaa@gmail.com>
2024-09-03 22:01:24 -0300

    bugButtonAddNewUser

--- src/app/pages/reactiveTailwind/forms-users-tailwind/forms-users-tailwind.component.ts
+++ src/app/pages/reactiveTailwind/forms-users-tailwind/forms-users-tailwind.component.ts
@@ -1,15 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-users-tailwind',
   templateUrl: './forms-users-tailwind.component.html',
   styleUrls: ['./forms-users-tailwind.component.css']
 })
-export class FormsUsersTailwindComponent {
+export class FormsUsersTailwindComponent implements OnInit {
 
-  constructor( private router: Router ){}
+  userForm: FormGroup;
+  userData: any[] = [];
+  isModalOpen: boolean = false;
+
+  constructor( private router: Router, private usersService: UsersService, private fb: FormBuilder ){
+    this.userForm = this.fb.group({
+      first_name: [''],
+      last_name: [''],
+      email: [''],
+      gender: [''],
+      hours: [''],
+    })
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.usersService.getUser().subscribe(res =>
+      this.userData = res
+    );
+  }
+
+  sendSubmit(){
+    console.log(this.userForm.value);
+    if(this.userForm.valid){
+      this.usersService.addUser(this.userForm.value).subscribe(() => {
+        // console.log(user);
+        // this.getUsers();
+        this.userForm.reset();
+        this.isModalOpen = false;
+      })
+    }
+  }
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  editData(): void {
+    this.usersService.editUser(this.userForm.value, this.userForm.value.id).subscribe((user) => {
+
+    })
+  }
+
+  deleteUsers(id: number): void {
+    this.usersService.deleteUser(id).subscribe(() => {
+      console.log(`user deleted: ${id}`);
+
+    })
+  }
 
   logOutTailwind(): void {
     const token = localStorage.removeItem('token');
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -14,4 +14,20 @@ export class UsersService {
   getLogin(email: string, password: string): Observable <any> {
     return this.http.post(`${this.apiUrl}/login`, {email, password});
   }
+
+  getUser(): Observable<any>{
+    return this.http.get(`http://localhost:3000/users`);
+  }
+
+  addUser(dataUser: any): Observable<any> {
+    return this.http.post(`http://localhost:3000/users`, dataUser)
+  }
+
+  deleteUser( id: number ): Observable<any> {
+    return this.http.delete(`http://localhost:3000/users/${id}`)
+  }
+
+  editUser( dataEdit: any, id: number ): Observable<any>{
+    return  this.http.put(`http://localhost:3000/users/${id}`, dataEdit);
+  }
 }
